a04c5457e6e275dc8c3352c17efe580c516c70a3
sparoh2 <sparoh2@gmail.com>
2018-08-08 11:49:58 +0900

    site commit

---
+++ tidwatches/js/ex.js
@@ -0,0 +1,11 @@
+$(function(){
+	$(document).HorizontalBlocks({
+		parentBox: "#wrapper",
+		block:"#pages>li",
+		firstBlock: 1,
+		scrollSpeed: "fast",
+		horizontalMenu: ".home_contents > .home_contents_inner > div",
+	});
+	SyntaxHighlighter.all();
+	$("body").css({"opacity": 1});
+});
