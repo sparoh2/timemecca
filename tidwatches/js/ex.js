7168312077e874c297c25afe1bd237d4386cd0d9
sparoh2 <sparoh2@gmail.com>
2018-08-10 18:24:19 +0900

    TID dev용 branch 생성

--- tidwatches/js/ex.js
+++ tidwatches/js/ex.js
@@ -2,11 +2,13 @@ var jQuery = $;
 $(function(){
 	$(document).HorizontalBlocks({
 		parentBox: "#wrapper",
-		block:"#pages>li",
+		block:"#pages > li",
 		firstBlock: 1,
 		scrollSpeed: "fast",
 		horizontalMenu: ".home_contents > .home_contents_inner > div",
+		start: function() {
+			console.log('sdadsads');
+		}
 	});
-	SyntaxHighlighter.all();
 	$("body").css({"opacity": 1});
 });
